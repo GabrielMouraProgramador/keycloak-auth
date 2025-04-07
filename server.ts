46c6f719b9f34ba059a9d18a74f73da6023270b6
Gabriel Moura <email.gabriel.moura@gmail.com>
2025-04-07 18:42:43 -0300

    fix: npm run dev

--- server.ts
+++ server.ts
@@ -1,12 +1,11 @@
+import { AuthRoutes } from "@/interfaces/routes/AuthRoutes";
 import fastify from "fastify";
 
 const server = fastify();
 
-server.get("/ping", async () => {
-  return "pong\n";
-});
+server.register(AuthRoutes, { prefix: "/" });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8000 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
