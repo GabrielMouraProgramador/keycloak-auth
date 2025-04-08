ab044a778e01ce3627e3ae6eb5e6997c3c00d519
Gabriel Moura <email.gabriel.moura@gmail.com>
2025-04-08 18:10:06 -0300

    fix: use case create realm

--- src/application/use-cases/auth/createRealmUseCase/useCase.ts
+++ src/application/use-cases/auth/createRealmUseCase/useCase.ts
@@ -1,13 +1,18 @@
+import { DomainError } from "@/domain/entities/DomainError";
 import ConsumerAuth from "../../../../domain/entities/ConsumerAuth";
 import { IClientAuthRepository } from "../../../../domain/repositories/IClientAuthRepository";
 
 export default class CreateRealmUseCase {
+  private readonly consumerListAllow: string[] = ["ADMIN", "API", "SITE"];
   constructor(private authRepository: IClientAuthRepository) {}
   async execulte(
     consumer: "ADMIN" | "API" | "DASHBOARD",
     cunsumer: ConsumerAuth,
     contractorId: string,
   ) {
+    if (!this.consumerListAllow.includes(consumer))
+      new DomainError("O Consumer não é valido");
+
     const realmName = `${consumer}-${contractorId}`;
     await this.authRepository.masterCreateRealm(realmName);
     await this.authRepository.createConsumer(realmName, cunsumer);
