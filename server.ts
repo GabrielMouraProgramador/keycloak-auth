220e5deb25b6c7f1cd3a5aecdebc9965ac13ebcf
Gabriel Moura <email.gabriel.moura@gmail.com>
2025-04-09 09:55:07 -0300

    feat: add cors controle

--- server.ts
+++ server.ts
@@ -1,9 +1,14 @@
 import { AuthRoutes } from "@/interfaces/routes/AuthRoutes";
 import fastify from "fastify";
+import cors from "@fastify/cors";
 
 const server = fastify();
 
 server.register(AuthRoutes, { prefix: "/" });
+
+await server.register(cors, {
+  origin: "*", // permite qualquer origem
+});
 
 server.listen({ port: 8000 }, (err, address) => {
   if (err) {
